25bda6b07f6fa9b9f19731af8e0fa8f6da8ac764
AntiKippi <24373791+AntiKippi@users.noreply.github.com>
2026-04-27 10:56:28 +0200

    Changed bind mount from only state file to whole state dir

--- entry.sh
+++ entry.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Activate tailscale connection
-tailscaled --state=/var/lib/tailscale/tailscaled.state > /var/log/tailscaled.out 2> /var/log/tailscaled.err &
+tailscaled --statedir=/var/lib/tailscale > /var/log/tailscaled.out 2> /var/log/tailscaled.err &
 
 # Start SOCKS5 server
 microsocks -p 1080 > /var/log/microsocks.out 2> /var/log/microsocks.err &
--- run.sh
+++ run.sh
@@ -7,11 +7,9 @@ CNT_NAME="tailsocks"
 # cd into the directory the script lives
 cd "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" || exit 1
 
-tailscale_state="$(realpath .)/tailscaled.state"
-
 docker build -t "$CNT_NAME" .
 docker run -ti \
-   --mount type=bind,source="$tailscale_state",target=/var/lib/tailscale/tailscaled.state \
+   --mount type=bind,source="$(realpath .)/statedir",target=/var/lib/tailscale/ \
    --hostname="$CNT_NAME" \
    --cap-add=NET_ADMIN \
    --cap-add=NET_RAW \
